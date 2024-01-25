23341e013eecf5ed817fddad5b251c6af7aaacea
speedytwenty <speedytwenty@gmail.com>
2024-01-25 05:14:12 -0700

    ZSH updates from nn

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,3 +22,46 @@ GREPEXCLUDE="$GREPEXCLUDE_DIRS $GREPEXCLUDE_FILES"
 alias grepr="grep $GREPEXCLUDE -r"
 # shortcut for grep recursive with case insensitivity
 alias grepri="grep $GREPEXCLUDE -ri"
+
+alias gdc="git diff --cached"
+
+# reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+alias lpath='echo $PATH | tr ":" "\n"' # list the PATH separated by new lines
+
+# Applications
+alias ios='open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+# remove broken symlinks
+alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
+
+
+# use exa if available
+if [[ -x "$(command -v exa)" ]]; then
+  alias ll="exa --icons --git --long"
+  alias l="exa --icons --git --all --long"
+else
+  alias l="ls -lah ${colorflag}"
+  alias ll="ls -lFh ${colorflag}"
+fi
+alias la="ls -AF ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
