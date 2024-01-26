d040c320ab40ab924ec766f2a6919de019d18705
speedytwenty <speedytwenty@gmail.com>
2024-01-26 00:02:59 -0700

    Wezterm keybinding refinements

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -71,7 +71,17 @@ local config = {
       action = act.ActivatePaneDirection("Left"),
     },
     {
+      key = "h",
+      mods = "CMD|ALT",
+      action = act.ActivatePaneDirection("Left"),
+    },
+    {
       key = "RightArrow",
+      mods = "CMD|ALT",
+      action = act.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "l",
       mods = "CMD|ALT",
       action = act.ActivatePaneDirection("Right"),
     },
@@ -81,15 +91,42 @@ local config = {
       action = act.ActivatePaneDirection("Up"),
     },
     {
+      key = "k",
+      mods = "CMD|ALT",
+      action = act.ActivatePaneDirection("Up"),
+    },
+    {
       key = "DownArrow",
+      mods = "CMD|ALT",
+      action = act.ActivatePaneDirection("Down"),
+    },
+    {
+      key = "j",
       mods = "CMD|ALT",
       action = act.ActivatePaneDirection("Down"),
     },
     --Scrolling
     { key = "UpArrow", mods = "ALT|SHIFT", action = act.ScrollByPage(-0.5) },
+    { key = "k", mods = "ALT|SHIFT", action = act.ScrollByPage(-0.5) },
     { key = "DownArrow", mods = "ALT|SHIFT", action = act.ScrollByPage(0.5) },
+    { key = "j", mods = "ALT|SHIFT", action = act.ScrollByPage(0.5) },
     { key = "UpArrow", mods = "ALT", action = act.ScrollByLine(-1) },
+    { key = "k", mods = "ALT", action = act.ScrollByLine(-1) },
     { key = "DownArrow", mods = "ALT", action = act.ScrollByLine(1) },
+    { key = "j", mods = "ALT", action = act.ScrollByLine(1) },
+    -- Turn off the default CMD-m Hide action, allowing CMD-m to
+    -- be potentially recognized and handled by the tab
+    { key = 'm', mods = 'CMD', action = act.DisableDefaultAssignment },
+    -- Disable closing window with CMD+W
+    --{ key = 'w', mods = 'CMD', action = act.DisableDefaultAssignment },
+    --Resize pane
+    { key = 'UpArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Up', 1 } },
+    { key = 'j', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Down', 2 } },
+    { key = 'DownArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Down', 2 } },
+    { key = 'RightArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Right', 4 } },
+    { key = 'l', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Right', 4 } },
+    { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Left', 3 } },
+    { key = 'h', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Left', 3 } },
   },
   window_padding = {
     left = 0,
